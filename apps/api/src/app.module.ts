1f1936979501e289eb35614c2bd6031af4c12672
Samir Maoude <samir.maoude@grenoble-inp.org>
2026-03-03 01:45:11 +0100

    setup database

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,37 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { KycModule } from './kyc/kyc.module';
-import { DeliveryPricingModule } from './delivery/delivery-pricing.module';
-import { RedisModule } from './redis/redis.module';
-import { StoreModule } from './store/store.module';
-import { OrderMerchantsModule } from './order/merchants/order-merchants.module'
-import { OrderLivreursModule } from './order/livreurs/order-livreurs.module'
-import { DriverMeModule } from './driver/driver-me.module'
-import { UploadModule } from './upload/upload.module'
-import { AdminModule } from './admin/admin.module'
-import { MerchantModule } from './merchant/merchant.module'
-import { TasksModule } from './tasks/tasks.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    AuthModule,
-    KycModule,
-    DeliveryPricingModule,
-    RedisModule,
-    StoreModule,
-    OrderLivreursModule,
-    OrderMerchantsModule,
-    DriverMeModule,
-    UploadModule,
-    AdminModule,
-    MerchantModule,
-    TasksModule
-  ],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
