be41531aa7cba259dd05c1272681579737d55a18
Dang Khoa <duong-dang-khoa.dang@grenoble-inp.org>
2026-03-04 16:22:26 +0100

    fix importation app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -9,5 +11,7 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
