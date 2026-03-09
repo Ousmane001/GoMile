40b44a22502e5448f38bb71b0881c02e4e834a8f
Dang Khoa <duong-dang-khoa.dang@grenoble-inp.org>
2026-03-09 07:39:51 +0100

    projet-specialite/gomile#12 debug token generation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,11 +7,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-      new ValidationPipe({
+    new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    })
+    }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
