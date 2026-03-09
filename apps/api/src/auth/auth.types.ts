9f5a451068ece489bb5b5a8f0b07435fa5152ee7
Dang Khoa <duong-dang-khoa.dang@grenoble-inp.org>
2026-03-09 09:33:46 +0100

    projet-specialite/gomile#9 Modify auth test, clean up

--- apps/api/src/auth/auth.types.ts
+++ apps/api/src/auth/auth.types.ts
@@ -1,5 +1,12 @@
 import { Role } from '@prisma/client';
 
+export interface JwtPayload {
+  sub: string;
+  email: string;
+  role: Role;
+  jti?: string;
+}
+
 export interface AuthenticatedUser {
   id: string;
   email: string;
