b540db213e43f99a5d403308feadab93f29d5529
Dang Khoa <duong-dang-khoa.dang@grenoble-inp.org>
2026-03-09 07:39:51 +0100

    projet-specialite/gomile#12 debug token generation

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterMerchantDto } from './dto/register-merchant.dto';
 import { RegisterDriverDto } from './dto/register-driver.dto';
