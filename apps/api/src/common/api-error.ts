7235868350718817647ceb1d1e97f2df0f2e711d
Duong Dang Khoa Dang <duong-dang-khoa.dang@grenoble-inp.org>
2026-04-11 23:44:07 +0200

    error factory refactor, shared contracts update

---
+++ apps/api/src/common/api-error.ts
@@ -0,0 +1,19 @@
+// Error factory
+export interface ApiErrorDefinition {
+  statusCode: number;
+  message: string;
+}
+
+export interface ApiErrorPayload <T extends string > {
+  code: T;
+  message: string;
+  statusCode: number;
+}
+
+export function createApiError<T extends string> (
+  code: T,
+  errors: Record<T, ApiErrorDefinition>,
+): ApiErrorPayload<T> {
+  const definition = errors[code];
+  return {code, message: definition.message, statusCode: definition.statusCode};
+}
