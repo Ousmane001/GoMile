c23f2b769e8c8afa5946a37f07cbee85f8e7989f
Duong Dang Khoa Dang <duong-dang-khoa.dang@grenoble-inp.org>
2026-04-14 15:46:53 +0200

    pickup logic handshake

--- apps/api/src/order/order-messages.ts
+++ apps/api/src/order/order-messages.ts
@@ -1,4 +1,5 @@
 export const ORDER_MESSAGE = {
   ORDER_CREATED : "Commande cree avec succes",
   ORDER_CANCELLED : "Commande annulée avec succès",
+  HANDSHAKE_ATTEMPS_FAILED : "Vous avez depasse le nombre de tentatives autorisees. Veuillez contacter votre fiable admin"
 }
