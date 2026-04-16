45ea9e4d88fd68feae108c425698770bc256dad1
Duong Dang Khoa Dang <duong-dang-khoa.dang@grenoble-inp.org>
2026-04-16 02:07:32 +0200

    driver api for mobile

---
+++ apps/api/src/driver/driver-me.message.ts
@@ -0,0 +1,3 @@
+export const DRIVER_MESSAGES = {
+  DRIVER_LOCATION_UPDATED: "Position du livreur mis a jour avec succes"
+} as const;
---
+++ apps/api/src/driver/driver-me.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DriverMeController } from './driver-me.controller';
+import { DriverMeService } from './driver-me.service';
+import { OrderLivreursModule } from '../order/livreurs/order-livreurs.module';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+
+@Module({
+  imports: [OrderLivreursModule],
+  controllers: [DriverMeController],
+  providers: [DriverMeService, JwtAuthGuard, RolesGuard],
+})
+export class DriverMeModule {}
