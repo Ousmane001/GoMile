0351be094638e36f2a557d7aea2f05d45f2fdb68
Duong Dang Khoa Dang <duong-dang-khoa.dang@grenoble-inp.org>
2026-04-28 03:06:38 +0200

    fix otp alignments

--- apps/api/src/emails/components/gomile-logo.tsx
+++ apps/api/src/emails/components/gomile-logo.tsx
@@ -4,6 +4,6 @@ const S3 = 'https://amzn-3-backend-kyc-gomiile-731322152450-eu-north-1-an.s3.eu-
 
 export function GomileLogo() {
   return (
-    <img src={`${S3}/gomile-logo.png`} width="32" height="32" alt="Gomile" style={{ display: 'block' }} />
+    <img src={`${S3}/gomile-logo-v2.png`} width="32" height="32" alt="Gomile" style={{ display: 'block' }} />
   )
 }
